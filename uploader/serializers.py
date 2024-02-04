4d1157327dd7678066cfa73a8d6bc2c0b1ae9efc
Илья Афанасьев <iafanasevmail@gmail.com>
2024-02-04 23:01:35 +0300

    changed serializers

--- uploader/serializers.py
+++ uploader/serializers.py
@@ -4,10 +4,17 @@ from uploader.models import File
 
 
 class FileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = File
         fields = (
+            'id',
             'file',
             'uploaded_at',
             'processed',
         )
+        read_only_fields = ('processed',)
+
+    def create(self, validated_data) -> File:
+        file = File.objects.create(file=validated_data['file'])
+        return file
